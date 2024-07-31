94157877c8fa600a78d1decf790be0d434915259
tinko223306 <valentinspiroski@gmail.com>
2024-07-31 17:23:30 +0200

    fix redirect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('express', { activePage: 'equipment' });
+  res.redirect('/equipment');
+  //res.render('express', { activePage: 'equipment' });
 });
 
 module.exports = router;
